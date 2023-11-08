195bfea7a523d869af6d28ff0c8f01e7d88eb064
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2023-11-08 10:02:53 +0200

    Task 1 Variables and Operators

--- firstExercise.js
+++ firstExercise.js
@@ -12,18 +12,28 @@
 // A. TASK 1:VARIABLES and OPERATORS
 // 1. Create a variable called “language” and assign it the value “JavaScript”. Then create
 // a variable called “score” and assign it the value “10”.
-
+ let language = 'Javascript';
+ let score = 10;
 // 2.Create two variables called “length” and “width”. Assign each variable a value of 10 and 5 respectfully. Use
 // the variables to calculate the area of a rectangle with the length and width specified (remember area = length x width).
+let length = 5;
+let width = 10;
+let area = 5*10;
 // Write the following to the console using CONCATENATION and BACKTICKS: 
 // “The area of the rectangle is …” where … is the area you calculated.
-
+console.log(`The area of the rectangle is ` + ( area));
 // 3.Create two variables called “num1” and “num2.” Assign each variable a value of 10 and 4 respectfully.
+let num1 = 10;
+let num2 = 4;
 // Calculate and display in the console what the remainder is if num1 is divided by num2.
-
+console.log(num1 % num2);
+console.log(num1 / num2)
 // 4.Using the variables you created in the previous step, output the results
 // of the division in the following format: “num1 / num2 = x remainder y.” HINT: Backticks
+let x = num1 / num2;
+let y = num1 % num2
 
+console.log((x) + ' remainder ' + (y));
 
 // B. Task 2:Data Types
 // 1. Define the following data types with your own contents[ 'integer', 'decimal', 'string', 'array'(minimum: 3 items), 'Object'(minimum: 4 values)]
