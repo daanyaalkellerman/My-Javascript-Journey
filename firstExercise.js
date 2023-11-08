a165092bad2c22f724cf6d81aee28c7b15be8f17
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2023-11-08 10:29:04 +0200

    data types

--- firstExercise.js
+++ firstExercise.js
@@ -27,21 +27,49 @@ let num1 = 10;
 let num2 = 4;
 // Calculate and display in the console what the remainder is if num1 is divided by num2.
 console.log(num1 % num2);
-console.log(num1 / num2)
+console.log(num1 / num2);
 // 4.Using the variables you created in the previous step, output the results
 // of the division in the following format: “num1 / num2 = x remainder y.” HINT: Backticks
 let x = num1 / num2;
-let y = num1 % num2
+let y = num1 % num2;
 
 console.log((x) + ' remainder ' + (y));
 
 // B. Task 2:Data Types
 // 1. Define the following data types with your own contents[ 'integer', 'decimal', 'string', 'array'(minimum: 3 items), 'Object'(minimum: 4 values)]
-// 2. Once you’ve created the variables, display them to the console
-// 3. Use the ‘typeof’ function to check each variable
-// 4. Create an array with numbers and strings. Comment what data type you expect this tobe. Then use the ‘typeof’ function on this array
-// 5. Create a null variable and an undefined variable
+let integer4 = 1
+let decimal3 = 4.5
+let string2 = 'Alexander'
+let array1 = [ 'Toyota' , 'Honda', 'Golf'  ];
 
+let oboject ={
+    name: 'Jeff',
+    ages:21,
+    city: 'Johannesburg',
+    country: 'Southyafrica',
+}
+let {name,ages,city,country} = oboject;
+
+// 2. Once you’ve created the variables, display them to the console
+    console.log(integer4)
+    console.log(decimal3)
+    console.log(string2)
+    console.log(array1)
+  console.log(oboject)
+// 3. Use the ‘typeof’ function to check each variable
+console.log(typeof integer4)
+console.log(typeof decimal3)
+console.log(typeof string2)
+console.log(typeof array1)
+console.log(typeof oboject)
+// 4. Create an array with numbers and strings. Comment what data type you expect this tobe. Then use the ‘typeof’ function on this array
+let Stringynum = ['i am', 30, 'years old', 21, 'is the twenty first number']
+console.log(typeof Stringynum)
+// 5. Create a null variable and an undefined variable
+let children = null;
+let age;
+
+console.log()
 // ***********C. Task 3. functions and contional statements************
 // N.B. READ WITH UNDERSTANDING!!!
 //  1.  Create 3 functions to convert R1000 into dollars, euros and pounds. Take function naming into consideration!
