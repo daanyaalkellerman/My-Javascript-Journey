5f01cec610316ca952c012a94fcc42f8d6dd306e
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2023-11-15 09:52:40 +0200

    other java

--- firstExercise.js
+++ firstExercise.js
@@ -223,6 +223,7 @@ array.splice(array.indexOf('jodi'),1)
 //to remove specific
 splice
 array.splice(2,0,'joel', 'unity')
+array[2]='6'
 //replaces  where do you want to delete it , how many do you want to delete, this is what youre replacing it with
 array.slice(0,3)
 //makes it into a new array
@@ -249,3 +250,207 @@ array.concat(string)
 //can use to bring another array and string 
 
 // document object model manipulation
+
+let clickMe = document.querySelector('[data-clickMe]')
+clickMe.addEventListener('click',alert())
+
+function alert(){
+    alert('this was pushed')
+}
+
+// //objects
+// //key and value
+// //key  = property (something about an object)
+// //objects set to a variable have to use braces{}
+// //object literals / declaration
+// let car = {
+//     name : 'Toyota',
+//     type : 'sprint',
+//     engineSize : '1.5 litre'
+// }
+
+// new keyword
+// overwrites
+let person = new Object
+person.name = 'Matthew'
+person.lastName = 'Brown'
+person.age = 23
+person.favFood = 'anything'
+//only use if youre working with others
+delete person.favFood
+
+// //object.create
+
+// // let blank = {
+// //     name: null,
+// //     surname: null,
+// //     age: null
+// // }
+
+// // let newBlank = Object.create(blank)
+// // newBlank.name = 'matthew'
+
+// //delete - use only on objects
+
+// //nested objects
+
+// const desktop = {
+//     cpu: 'Ryzen 5',
+//     ram: '8 gigs',
+//     hard_drive: {
+//         first: '1000gigs',
+//         second: '500gigs'
+//     }
+// }
+// // desktop.hard_drive.first to access the first object within the object
+
+// //nested objects in arrays
+
+// const myObj = {
+//     name: 'John',
+//     age: 60,
+//     cars: [
+//         {
+//             name:'Ford',
+//             models: ['fiesta','focus','mustang']
+//         },
+//         {
+//             name:'BMW',
+//             models:['320','X3','X5']
+//         },
+//         {
+//             name: 'Fiat',
+//             models:['500','Panda']
+//         }
+//     ]
+// }
+
+//object method - function inside the object
+//function inside of an object is a method
+// const person = {
+//     firstName: 'Johna',
+//     lastName: 'doee',
+//     id: 445,
+//     fullName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+// //this is a keyword in javascript that tells your function that you want to use this first name this last name (global scoping) (only works within objects)
+
+// //will print info to the page
+
+// const person = {
+//     firstName: 'Johna',
+//     lastName: 'doee',
+//     id: 445,
+//     fullName: function(){
+//         return (this.firstName + " " + this.lastName)
+//         .toUpperCase();
+//     }
+// };
+// document.querySelector('h1').innerText = person.fullName();
+
+// //for in loop
+// //have to use object expression where p is used as index with [], square brackets
+// // for (let p in person){
+// //     console.log(person[p])
+// // }
+
+// //nested loop to access information in the cars
+
+// for(i in myObj.cars){
+//     console.log(myObj.cars[i].name)
+//     for(x in myObj.cars[i].models){
+//         console.log(myObj.cars[i].models[x]);
+//     }
+// }
+
+// //factory function object RETURN an object
+
+// function createPerson(firstName, lastName, age){
+//     let myObjeje={
+//          firstName: firstName,
+//         lastName: lastName,
+//         age: age,
+       
+//     }
+//     return myObjeje
+//     //still works both ways
+//     // return {
+//     //     firstName: firstName,
+//     //     lastName: lastName,
+//     //     age: age,
+//     // }
+// }
+// // function createPerson(){
+// //     let myObjeje={
+// //          firstName: 'namename',
+// //         lastName: 'surname',
+// //         age: 21,
+       
+// //     }
+// //     return myObjeje
+
+// //you dont need the [new] keyword
+
+// let person4 =createPerson('danny','idk',32)
+// let personActions= {
+//     getfullName(){
+//         return this.firstName + ''+this.lastName
+//     }
+// }
+// let person3 =createPerson('matthew', 'brown', 23,personActions.getfullName())
+// // [this] is a constructer function 
+
+
+// function carDescription(brand,year,model,colour){
+//     this.brand = brand
+//     this.year = year;
+//     this.model = model;
+//     this.colour = colour;
+// }
+
+// let car1= new carDescription('porsche','GT3',2013,'white')
+
+
+//switch statement cont.
+//to check multiple values
+let mark = 100
+let condition = true
+switch(condition){
+    case 100:
+        console.log('This is full marks');
+            break
+    case(mark>80):
+        console.log('this is great');
+             break
+    case (mark>70):
+            console.log('This is good');
+            break
+            default:
+            console.log('This is low ');
+}
+
+//while loop
+let result = ''
+let i = 0
+// while (i <= 10){
+//     console.log("The number is " + i);
+//     i++
+// }
+
+//do while loop
+
+do{
+    i = i + 1;
+    result = result + i
+} while (i < 5)
+
+console.log(result);
+
+let laptop = {
+    name: 'something',
+    battery: '3999'
+};
+
+let (name, battery) = laptop
